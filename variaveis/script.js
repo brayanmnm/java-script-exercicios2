79dd4ec6a888cc1c25b9cb79f19ec379f6c5873d
Brayan Silva Andrade Miguel <brayanmiguelred@gmail.com>
2024-10-14 18:45:00 -0300

    operacoes variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,28 @@
+// isso e um comentario 
+
+// tipos premitivos
+
+// number 
+let pi = 3.14
+console.log(` A variavel " + pi + " e do tipo ${ typeof(pi) } ` ) 
+
+// string
+let nome = "pafuncio"
+console.log( `A variavel ${nome} é do tipo ${ typeof(nome) }` )
+
+// boolean
+let palmeirascampeao = true 
+console.log( `a variavel ${palmeirascampeao} e do tipo ${ typeof (palmeirascampeao)}`)
+
+// object
+
+let frutas = [ "pera", "uva", "maça" ]
+
+console.log( `a variavel ${frutas} e do tipo ${ typeof (frutas)}`)
+
+function xablau () {
+    console.log("oi sou uma funçao xablau")
+}
+ 
+let yeah = function (){}
+console.log( `a variavel ${yeah} e do tipo ${ typeof (yeah)}`)
